45800c12c22b8e989d9d5e91f3c13041a74ffa67
Alexander Lukakhin <la@snov.io>
2022-05-23 18:08:24 +0300

    boptions api

--- src/components/CounterNumber.vue
+++ src/components/CounterNumber.vue
@@ -8,30 +8,7 @@
 import { defineComponent, isVue2 } from 'vue-demi';
 
 export default defineComponent({
-  name: 'Awesome',
-  props: {
-    from: {
-      type: [Number, String],
-      default: 1,
-    },
-    to: {
-      type: [Number, String],
-      required: true,
-      default: 0,
-    },
-    duration: {
-      type: Number,
-      default: 1000, // Duration of animation in ms
-    },
-    easing: {
-      type: String,
-      default: 'easeInOutQuad',
-    },
-    delay: {
-      type: Number,
-      default: 0, // Delay the animation in ms
-    },
-  },
+  name: 'Awesomennnn',
   data() {
     return {
       count: 33,
@@ -44,20 +21,10 @@ export default defineComponent({
   },
   mounted() {
     if (isVue2) {
-      console.log('is vue 2')
+      console.log('is vue 2!')
     } else {
       console.log('else-ssssss')
     }
   }
-  // setup(props, ctx) {
-  //   const { count } = useCounter(props, ctx.emit);
-  //   const clickFun = (): void => {
-  //     count.value = count.value + 1;
-  //   }
-  //   return {
-  //     count,
-  //     clickFun,
-  //   }
-  // },
 })
 </script>
