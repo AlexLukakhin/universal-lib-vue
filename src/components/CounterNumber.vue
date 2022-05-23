0d4daf3dcab47c3db674fe6e2af6e415e4c7b405
Alexander Lukakhin <la@snov.io>
2022-05-23 19:35:55 +0300

    boptions api

--- src/components/CounterNumber.vue
+++ src/components/CounterNumber.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    <span class="counter-number">test</span>
-    <button>click</button>
+    <span class="counter-number">{{count}}</span>
+    <button @click="clickFun">click</button>
   </div>
 </template>
 <script>
-import { defineComponent, isVue2, ref } from 'vue-demi';
+import { isVue2, ref } from 'vue-demi';
 
-export default defineComponent({
+export default {
   name: 'Counter',
-})
+  setup() {
+    // const { count } = useCounter(props, ctx.emit);
+    if (isVue2) {
+      console.log('is vue 2!!')
+    } else {
+      console.log('else-ssssss')
+    }
+    const count = ref(11);
+    const clickFun = () => {
+      count.value = count.value + 101;
+    }
+    return {
+      count,
+      clickFun,
+    }
+  },
+}
 </script>
